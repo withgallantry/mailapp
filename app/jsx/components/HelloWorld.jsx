53cea4e2c73d544a54c98f459ce24049937e89cd
Matthew Gallantry <gallantrym@infrastructures-MacBook-Pro.local>
2015-10-09 11:10:18 +0100

    Initial Push

--- app/jsx/components/HelloWorld.jsx
+++ app/jsx/components/HelloWorld.jsx
@@ -1,12 +1,13 @@
 /* @jsx React.DOM */
 
-var React = require('react');
+import React from 'react';
 
 module.exports = React.createClass({
+
   render: function() {
     return (
       <div>
-        <h1>O, hey there</h1>
+        <h1>O, hey there Matty</h1>
         <p>You can find this text in <code>app/components/HelloWorld.jsx</code>. It's pulled in by <code>app/index.jsx</code></p>
       </div>
     );
